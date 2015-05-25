e091e36d47161c0341999c32d09064b7602bc52b
Ian McConachie <imcconachie@novarumdx.com>
2015-05-25 11:31:11 +0100

    add function to print results in readable manner

--- parse_access_log.py
+++ parse_access_log.py
@@ -20,15 +20,27 @@ def log_data(line_data):
         mb_sent = int(line_data['response_bytes_clf'])
 
     if line_data['status'][0] == '2':
-        success = True
+        totals[time]['success'] += 1
     else:
-        success = False
+        totals[time]['error'] += 1
 
     totals[time]['count'] += 1
     totals[time]['response_time'] += response_time
     totals[time]['mb_sent'] += mb_sent
 
-    return success
+def print_results(totals):
+    '''
+    Function to print the data gleaned from the access log
+    in a easily readable format
+    '''
+    for key in sorted(totals):
+        mean_response_time = totals[key]['response_time'] / totals[key]['count']
+
+        print '%s:' % key
+        print '    Requests: %s successful  %s error' % (totals[key]['success'], totals[key]['error'])
+        print '    Mean response time: %s  MBs sent: %s' % (mean_response_time, totals[key]['mb_sent'])
+
+
 
 log = open('web-access.log', 'r')
 
@@ -39,19 +51,15 @@ for line in log.readlines():
     if time not in totals:
         totals[time] = {
             'count': 0,
+            'success': 0,
+            'error': 0,
             'response_time': 0,
             'mb_sent': 0
             }
-        if log_data(line_data):
-            successful_requests += 1
-        else:
-            error_requests += 1
-    else:
-        if log_data(line_data):
-            successful_requests += 1
-        else:
-            error_requests += 1
+    log_data(line_data)
+
+
+print_results(totals)
 
 
 
-import pdb; pdb.set_trace()
