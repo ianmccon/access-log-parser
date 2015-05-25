67f563b14f1779c97ed6bbc518929485c01362e5
Ian McConachie <imcconachie@novarumdx.com>
2015-05-25 11:02:02 +0100

    add try except for mb_sent to trap "-" entry

--- parse_access_log.py
+++ parse_access_log.py
@@ -9,8 +9,15 @@ successful_requests = 0
 error_requests = 0
 
 def log_data(line_data):
+    '''
+    Function to pull values from parsed log line
+    and saved cumulative values to totals dictionary
+    '''
     response_time = int(line_data['time_us'])
-    mb_sent = int(line_data['response_bytes_clf'])
+    if line_data['response_bytes_clf'] == '-':
+        mb_sent = 0
+    else:
+        mb_sent = int(line_data['response_bytes_clf'])
 
     if line_data['status'][0] == '2':
         success = True
@@ -23,7 +30,7 @@ def log_data(line_data):
 
     return success
 
-log = open('log.log', 'r')
+log = open('web-access.log', 'r')
 
 for line in log.readlines():
     line_data = line_parser(line)
@@ -37,16 +44,14 @@ for line in log.readlines():
             }
         if log_data(line_data):
             successful_requests += 1
-
         else:
             error_requests += 1
     else:
         if log_data(line_data):
             successful_requests += 1
-
         else:
             error_requests += 1
 
 
 
-
+import pdb; pdb.set_trace()
