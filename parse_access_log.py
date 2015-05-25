825d760943fe7d82334f7a02a301c93f6a75f9dd
Ian McConachie <imcconachie@novarumdx.com>
2015-05-25 10:51:35 +0100

    Parsing of log file with apache_log_parser and logging function

--- parse_access_log.py
+++ parse_access_log.py
@@ -1,1 +1,52 @@
-from . import apache_log_parser
+from apache_log_parser import make_parser
+from pprint import pprint
+
+line_parser = make_parser('%a %l %u %t \"%r\" %>s %b %D')
+
+totals = {}
+
+successful_requests = 0
+error_requests = 0
+
+def log_data(line_data):
+    response_time = int(line_data['time_us'])
+    mb_sent = int(line_data['response_bytes_clf'])
+
+    if line_data['status'][0] == '2':
+        success = True
+    else:
+        success = False
+
+    totals[time]['count'] += 1
+    totals[time]['response_time'] += response_time
+    totals[time]['mb_sent'] += mb_sent
+
+    return success
+
+log = open('log.log', 'r')
+
+for line in log.readlines():
+    line_data = line_parser(line)
+    time = line_data['time_received_datetimeobj'].strftime("%d-%m-%Y %H:%M")
+
+    if time not in totals:
+        totals[time] = {
+            'count': 0,
+            'response_time': 0,
+            'mb_sent': 0
+            }
+        if log_data(line_data):
+            successful_requests += 1
+
+        else:
+            error_requests += 1
+    else:
+        if log_data(line_data):
+            successful_requests += 1
+
+        else:
+            error_requests += 1
+
+
+
+
